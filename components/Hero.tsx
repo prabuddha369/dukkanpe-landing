ca80e9f2addaf45e14e1198d6c13afb56b3ac725
prabuddha369 <prabuddhapiku@gmail.com>
2024-11-30 12:05:52 +0530

    minor change in text size

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,24 +2,24 @@ import { IoIosSearch } from "react-icons/io";
 import React from "react";
 
 const HeroSection: React.FC = () => (
-  <section className="flex flex-col items-center text-center bg-gray-100 py-28 px-6">
+  <section className="flex flex-col items-center text-center bg-gray-100 pt-28 md:py-28 px-6">
     <div
-      className="w-[800px] h-[300px] md:w-[1300px] md:h-[800px] rounded-xl"
+      className="w-[400px] h-[300px] md:w-[1300px] md:h-[800px] rounded-xl"
       style={{
         backgroundImage: `url("/hero.jpg")`,
         backgroundSize: "100% 100%",
         backgroundRepeat: "no-repeat",
       }}
     >
-      <div className="bg-[#00000080] w-full h-full flex flex-col justify-end pb-40 items-center gap-5">
-        <h1 className="md:text-4xl text-2xl font-bold text-blackText">
+      <div className="bg-[#00000080] w-full h-full flex flex-col justify-end pb-5 md:pb-40 items-end md:items-center md:gap-5">
+        <h1 className="md:text-4xl mx-4 text-2xl font-bold text-blackText">
           Welcome to DookanPe
         </h1>
-        <p className="text-white mb-10 text-end">
+        <p className="text-white mb-2 mx-4 md:mb-10 text-end md:text-xl text-xs w-2/3 md:w-fit">
           A commerce platform connecting small to medium shops with their raw
           materials.
         </p>
-        <div className="flex justify-between items-center text-base md:text-xl rounded-lg border border-gray-300 bg-white">
+        <div className="flex mx-4 justify-between items-center text-base md:text-xl rounded-lg border border-gray-300 bg-white">
           <IoIosSearch size={30} className="text-gray-500 md:text-xl mx-4" />
           <input
             type="email"
